c9aa7a4b57ec9fdcc9d5f8608ab4330ab95b4c68
Sellpixels Robot <nick@mediatec.org>
2021-02-19 18:05:49 +0200

    widgets wip (peg)

---
+++ src/app/@vb/widgets/AppPartials/Calendar/calendar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'vb-app-partials-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss'],
+})
+export class VbAppPartialsCalendarComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
